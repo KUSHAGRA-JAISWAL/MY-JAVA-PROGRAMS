429360452ccb1dd3bbf062e342d5ef17515ee228
KUSHAGRA-JAISWAL <kushagrajaiswal00@gmail.com>
2023-04-20 22:15:06 +0530

    Added assignment Codes

--- src/assignments/cat/set_B/Passanger.java
+++ src/assignments/cat/set_B/Passanger.java
@@ -18,20 +18,20 @@ package src.assignments.cat.set_B;
 
 import java.util.Scanner;
 
-class InvalidPNRNumber extends Exception{
-    public InvalidPNRNumber(String message){
+class InvalidPNRNumber extends Exception {
+    public InvalidPNRNumber(String message) {
         super(message);
     }
 }
 
-class NameCaseException extends Exception{
-    public NameCaseException(String message){
+class NameCaseException extends Exception {
+    public NameCaseException(String message) {
         super(message);
     }
 }
 
-class InvalidAgeException extends Exception{
-    public InvalidAgeException(String message){
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
         super(message);
     }
 }
@@ -41,62 +41,60 @@ public class Passanger {
     private int age;
     private String PNR;
 
-    public Passanger(String name, int age, String PNR){
+    public Passanger(String name, int age, String PNR) {
         this.name = name;
         this.age = age;
         this.PNR = PNR;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public void setAge(int age){
+    public void setAge(int age) {
         this.age = age;
     }
 
-    public int getAge(){
+    public int getAge() {
         return this.age;
     }
 
-    public void setPNR(String PNR){
+    public void setPNR(String PNR) {
         this.PNR = PNR;
     }
 
-    public String getPNR(){
+    public String getPNR() {
         return this.PNR;
     }
 
-    public void checkPNR() throws InvalidPNRNumber{
-        if(this.PNR.length() != 11){
+    public void checkPNR() throws InvalidPNRNumber {
+        if (this.PNR.length() != 11) {
             throw new InvalidPNRNumber("PNR number should be of 11 characters");
         }
-        if(this.PNR.charAt(3) != '-'){
+        if (this.PNR.charAt(3) != '-') {
             throw new InvalidPNRNumber("PNR number should have a hyphen at the fourth position");
         }
     }
 
-    public void checkName() throws NameCaseException{
-        for(int i = 0; i < this.name.length(); i++){
-            if(this.name.charAt(i) >= 'a' && this.name.charAt(i) <= 'z'){
-                throw new NameCaseException("Name should be in upper case");
-            }
+    public void checkName() throws NameCaseException {
+        if (this.name != this.name.toUpperCase()) {
+            throw new NameCaseException("Name should be in upper case");
         }
     }
 
-    public void checkAge() throws InvalidAgeException{
-        if(this.age < 0){
+    public void checkAge() throws InvalidAgeException {
+        if (this.age < 0) {
             throw new InvalidAgeException("Age should be a positive number");
         }
     }
 
 }
 
-class Driver{
+class Driver {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter name: ");
@@ -105,19 +103,19 @@ class Driver{
         int age = sc.nextInt();
         System.out.print("Enter PNR: ");
         String PNR = sc.next();
-    
+
         Passanger p = new Passanger(name, age, PNR);
-        try{
+        try {
             p.checkPNR();
             p.checkName();
             p.checkAge();
-        }catch(InvalidPNRNumber e){
+        } catch (InvalidPNRNumber e) {
             System.out.println(e.getMessage());
-        }catch(NameCaseException e){
+        } catch (NameCaseException e) {
             System.out.println(e.getMessage());
-        }catch(InvalidAgeException e){
+        } catch (InvalidAgeException e) {
             System.out.println(e.getMessage());
-        }finally{
+        } finally {
             System.out.println("Check the Passanger details carefully: ");
             System.out.println("Name: " + p.getName());
             System.out.println("Age: " + p.getAge());
