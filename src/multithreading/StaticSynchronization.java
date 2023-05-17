9b69c0dd1a92e6e0d743f3276e3916cf4e2fc7be
KUSHAGRA-JAISWAL <kushagrajaiswal00@gmail.com>
2023-05-17 20:04:21 +0530

    Added Multithreading Section With all the Codes

--- src/multithreading/StaticSynchronization.java
+++ src/multithreading/StaticSynchronization.java
@@ -13,30 +13,38 @@ package src.multithreading;
 
 class Bank extends Thread {
     // user defined class Bank
+    // Now Bank class has one lock and 2 threads are sharing the same object.
     // Thread is a predefined class which is available in java.lang package.
 
-    int balance = 5000;
-    int widthdrawlAmount;
+    static int balance = 5000;
+    static int widthdrawlAmount;
 
     Bank(int widthdrawlAmount) {
         // parameterized constructor
         this.widthdrawlAmount = widthdrawlAmount;
     }
 
-    public void run() {
-        // run method
-        // run method is a predefined method which is available in Thread class.
-        // run method is used to execute the thread.
+    public static synchronized void withdraw() {
+        // static synchronized method
 
         String name = Thread.currentThread().getName();
         // getName() is a predefined method which is used to get the name of the thread.
 
         if (widthdrawlAmount <= balance) {
-            System.out.println(name + " Withdrawn Amount");
+            System.out.println(name + " Withdrawn Amount :" + widthdrawlAmount);
             balance = balance - widthdrawlAmount;
         } else {
-            System.out.println(name + " Insufficient Balance");
+            System.out.println(name + " Insufficient Balance :" + balance);
         }
+    }
+
+    public void run(){
+        // run method
+        // run method is a predefined method which is available in Thread class.
+        // run method is used to execute the thread.
+
+        withdraw();
+        // here we are calling the withdraw method of the class Bank.
     }
 }
 
@@ -54,11 +62,27 @@ class Customer {
         // here we are creating 2 threads which are sharing the same object.
 
         t1.setName("Kushagra");
-        t2.setName("Jaiswal");
+        t2.setName("Harshit");
         // here we are setting the name of the thread.
 
         t1.start();
         t2.start();
+        // here we are calling the start method of the class Bank.
+
+        Bank obj2 = new Bank(5000);
+        // here we are creating an object of the class Bank.
+        // object has 1 lock.
+
+        Thread t3  = new Thread(obj2);
+        Thread t4  = new Thread(obj2);
+        // here we are creating 2 threads which are sharing the same object.
+
+        t3.setName("Harsh");
+        t4.setName("Deepak");
+        // here we are setting the name of the thread.
+
+        t3.start();
+        t4.start();
         // here we are calling the start method of the class Bank.
         
 
